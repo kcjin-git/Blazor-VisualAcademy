f2e7d3d22501fc6cfdf8a8a2bca2041c3fc94f70
진광철 <tokcjin@gmail.com>
2020-07-16 23:07:05 +0900

    초기 커밋.

---
+++ VisualAcademy/Pages/About.razor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VisualAcademy.Pages
+{
+    public partial class About
+    {
+        private String _title = "Test About";
+
+        private string Title 
+        { 
+            get { return _title; } 
+            set { _title = value; } 
+        }
+
+        protected override void OnInitialized()
+        {
+
+        }
+    }
+}
---
+++ VisualAcademy/Pages/Portfolios/PortfolioWebsite.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VisualAcademy.Pages.Portfolios
+{
+    public partial class  PortfolioWebsite
+    {
+        [Inject]
+        public NavigationManager NavigationManagerRef { get; set; 
+        }
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+
+        protected void GotoDotNetKorea()
+        {
+            NavigationManagerRef.NavigateTo("/About");
+        }
+
+    }
+}
